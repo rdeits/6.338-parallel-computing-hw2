8330adda90a763d2c814e72ba300202789709cd6
Robin Deits <robin.deits@gmail.com>
2015-10-12 21:36:53 -0400

    10x speedup

--- ImageSeam/src/ImageSeam.jl
+++ ImageSeam/src/ImageSeam.jl
@@ -2,6 +2,7 @@ module ImageSeam
 
 using Images
 import FixedPointNumbers
+import ColorTypes
 import Base.size
 
 
@@ -24,7 +25,7 @@ function brightness(I::Image)
   b
 end
 
-function xenergy(b, x, y)
+function xenergy(b::Array{Float32, 2}, x::Int64, y::Int64)
   b[x-1, y] + 2b[x, y] + b[x+1, y]
 end
 
@@ -32,7 +33,7 @@ end
 #               [xenergy(b, x, y) for x=2:size(b, 1)-1, y=1:size(b, 2)];
 #               fill(Inf32, 1, size(b, 2))]
 
-function yenergy(b, x, y)
+function yenergy(b::Array{Float32, 2}, x::Int64, y::Int64)
   b[x, y-1] + 2b[x, y] + b[x, y+1]
 end
 
@@ -40,7 +41,7 @@ end
 #               [yenergy(b, x, y) for x=1:size(b, 1), y=2:size(b, 2)-1],
 #               fill(Inf32, size(b, 1), 1))
 
-function energy(xenergy, yenergy, x, y)
+function energy(xenergy::Array{Float32}, yenergy::Array{Float32}, x::Int64, y::Int64)
   sqrt((xenergy[x, y-1] - xenergy[x, y+1])^2 + (yenergy[x-1, y] - yenergy[x+1, y])^2)
 end
 
@@ -56,26 +57,26 @@ end
 #   e
 # end
 
-# the 3x3 stencil for energy
-function stencil(b)
-      xenergy = b[1,1]+2b[2,1]+b[3,1]-b[1,3]-2b[2,3]-b[3,3]
-      yenergy = b[1,1]+2b[1,2]+b[1,3]-b[3,1]-2b[3,2]-b[3,3]
-      √(xenergy^2 + yenergy^2)
-end
+# # the 3x3 stencil for energy
+# function stencil(b)
+#       xenergy = b[1,1]+2b[2,1]+b[3,1]-b[1,3]-2b[2,3]-b[3,3]
+#       yenergy = b[1,1]+2b[1,2]+b[1,3]-b[3,1]-2b[3,2]-b[3,3]
+#       √(xenergy^2 + yenergy^2)
+# end
 
-# energy of an array of brightness values 
-# input: assumed zero borders
-# output: left and right set to ∞
-function energy(b)
-  w, h = size(b)
-  e = [Float32(stencil( b[x-1:x+1, y-1:y+1] )) for x=2:w-1,y=2:h-1]
-  infcol = fill(Inf32,1,h-2)
-  [infcol; e; infcol]
-end
+# # energy of an array of brightness values 
+# # input: assumed zero borders
+# # output: left and right set to ∞
+# function energy(b)
+#   w, h = size(b)
+#   e = [Float32(stencil( b[x-1:x+1, y-1:y+1] )) for x=2:w-1,y=2:h-1]
+#   infcol = fill(Inf32,1,h-2)
+#   [infcol; e; infcol]
+# end
 
 
 type CarvableImage
-  img::Image
+  img::Image{ColorTypes.RGB4{FixedPointNumbers.UfixedBase{UInt8,8}}, 2, Array{ColorTypes.RGB4{FixedPointNumbers.UfixedBase{UInt8,8}}, 2}}
   width::Int32
   height::Int32
   brightness::Array{Float32, 2}
@@ -100,6 +101,9 @@ type CarvableImage
     compute_xenergy!(obj);
     compute_yenergy!(obj);
     compute_energy!(obj);
+    obj.cost_to_go[1,:] = Inf32;
+    compute_cost_to_go!(obj);
+    compute_seam!(obj);
 
     # carve!(obj);
     # @assert size(obj.energy) == size(energy(obj.brightness));
@@ -115,16 +119,44 @@ type CarvableImage
 end
 
 function compute_xenergy!(obj::CarvableImage)
-  for x = 2:obj.width+1
-    for y = 1:obj.height+2
+  for y = 1:obj.height+2
+    for x = 2:obj.width+1
+      obj.xenergy[x,y] = xenergy(obj.brightness, x, y)
+    end
+  end
+end
+
+function update_xenergy!(obj::CarvableImage)
+  s::Int64 = 0
+  for y = 1:obj.height+2
+    if y >= 2 && y <= obj.height
+      s = obj.seam[y]
+    else
+      s = 1
+    end
+    for x = max(s - 2, 2):obj.width+1
       obj.xenergy[x,y] = xenergy(obj.brightness, x, y)
     end
   end
 end
 
 function compute_yenergy!(obj::CarvableImage)
-  for x = 1:obj.width+2
-    for y = 2:obj.height+1
+  for y = 2:obj.height+1
+    for x = 1:obj.width+2
+      obj.yenergy[x,y] = yenergy(obj.brightness, x, y)
+    end
+  end
+end
+
+function update_yenergy!(obj::CarvableImage)
+  s::Int64 = 0
+  for y = 2:obj.height+1
+    if y >= 2 && y <= obj.height
+      s = obj.seam[y]
+    else
+      s = 1
+    end
+    for x = max(s - 2, 1):obj.width+2
       obj.yenergy[x,y] = yenergy(obj.brightness, x, y)
     end
   end
@@ -133,29 +165,55 @@ end
 function compute_energy!(obj::CarvableImage)
   obj.energy[1,:] = Inf32
   obj.energy[obj.width+2,:] = Inf32
-  for x = 2:obj.width+1
-    for y = 2:obj.height+1
+  for y = 2:obj.height+1
+    for x = 2:obj.width+1
       obj.energy[x, y-1] = energy(obj.xenergy, obj.yenergy, x, y)
     end
   end
 end
 
-function update_cost_to_go!(obj::CarvableImage)
-  obj.cost_to_go[:,end] = obj.energy[:,end]
-  obj.cost_to_go[1,:] = Inf32
-  obj.cost_to_go[obj.width+2,:] = Inf32
-  for y = obj.height-1:-1:1
-    for x = 2:obj.width+1
-      s, obj.directions[x-1,y] = findmin(obj.cost_to_go[x+[-1, 0, 1], y+1])
-      obj.cost_to_go[x,y] = obj.energy[x,y] + s
+function update_energy!(obj::CarvableImage)
+  obj.energy[1,:] = Inf32
+  obj.energy[obj.width+2,:] = Inf32
+  s::Int64 = 0
+  for y = 2:obj.height+1
+    if y >= 2 && y <= obj.height
+      s = obj.seam[y]
+    else
+      s = one(s)
+    end
+    for x = max(s - 2, 2):obj.width+1
+      obj.energy[x, y-1] = energy(obj.xenergy, obj.yenergy, x, y)
     end
   end
 end
 
-function update_seam!(obj::CarvableImage)
-  obj.seam[1] = indmin(obj.cost_to_go[2:obj.width+1, 1])
+function compute_cost_to_go!(obj::CarvableImage)
+  obj.cost_to_go[:,end] = obj.energy[:,end]
+  obj.cost_to_go[obj.width+2,:] = Inf32
+  search_directions = Int64[-1, 0, 1]
+  for y = obj.height-1:-1:1
+    for x = 2:obj.width+1
+      s_min = obj.cost_to_go[x-1, y+1]
+      obj.directions[x-1,y] = 1
+      for i = 2:3
+        s = obj.cost_to_go[x + search_directions[i], y+1]
+        if s < s_min
+          s_min = s
+          obj.directions[x-1,y] = i
+        end
+      end
+      obj.cost_to_go[x,y] = obj.energy[x,y] + s_min
+    end
+  end
+end
+
+function compute_seam!(obj::CarvableImage)
+  s = indmin(obj.cost_to_go[2:obj.width+1, 1])
+  obj.seam[1] = s
   for y=2:obj.height
-    obj.seam[y] = obj.seam[y-1] + obj.directions[obj.seam[y-1], y-1] - 2
+    s += obj.directions[s, y-1] - 2
+    obj.seam[y] = s
   end
 end
 
@@ -172,27 +230,13 @@ function get_image(obj::CarvableImage)
 end
 
 function carve!(obj::CarvableImage)
-  compute_xenergy!(obj)
-  compute_yenergy!(obj)
-  compute_energy!(obj)
-  update_cost_to_go!(obj)
-  update_seam!(obj)
   remove_seam!(obj)
+  update_xenergy!(obj)
+  update_yenergy!(obj)
+  update_energy!(obj)
+  compute_cost_to_go!(obj)
+  compute_seam!(obj)
 end
-
-
-# function cost_to_go(e)
-#   w, h = size(e)
-#   cost_to_go = copy(e)
-#   dirs = zeros(Int8, w-2, h-1)
-#   for y = h-1:-1:1
-#     for x = 2:w-1
-#       s, dirs[x-1,y] = findmin(cost_to_go[x+[-1, 0, 1], y+1])
-#       cost_to_go[x,y] += s
-#     end
-#   end
-#   cost_to_go, dirs
-# end
 
 #  e (row                  e[x,y] 
 #  dirs:                ↙   ↓   ↘       <--directions naturally live between the rows
@@ -202,84 +246,91 @@ end
 
 
 # Take an array of energies and work up from bottom to top accumulating least energy to bottom
-function least_energy(e)
-   # initialize dirs 
-   w, h = size(e)
-   dirs = fill(0, w-2, h-1) # w-2 because we don't need the infs, h-1 because arrows between rows
-   # compute
-   for y=h-1:-1:1, x=2:w-1          
-        s, dirs[x-1,y] = findmin(e[x+[-1, 0, 1], y+1]) # findmin gets the min and the index
-        e[x,y] += s   #  add in current energy +  smallest from below
-   end
-   e[2:w-1,1], dirs  # return top row without infinities and dirs
-end
+# function least_energy(e)
+#    # initialize dirs 
+#    w, h = size(e)
+#    dirs = fill(0, w-2, h-1) # w-2 because we don't need the infs, h-1 because arrows between rows
+#    # compute
+#    for y=h-1:-1:1, x=2:w-1          
+#         s, dirs[x-1,y] = findmin(e[x+[-1, 0, 1], y+1]) # findmin gets the min and the index
+#         e[x,y] += s   #  add in current energy +  smallest from below
+#    end
+#    e[2:w-1,1], dirs  # return top row without infinities and dirs
+# end
 
-function get_seam(dirs,x)
-  seam = fill(0,1+size(dirs,2))
-  seam[1]=x
-  for y=1:size(dirs,2)
-    seam[y+1] = seam[y] + dirs[seam[y],y] - 2
-  end
-  seam
-end
+# function get_seam(dirs,x)
+#   seam = fill(0,1+size(dirs,2))
+#   seam[1]=x
+#   for y=1:size(dirs,2)
+#     seam[y+1] = seam[y] + dirs[seam[y],y] - 2
+#   end
+#   seam
+# end
 
-using Colors
-#Mark a seam
-function mark_seam(img, seam, color=RGB4{U8}(1,1,1))
-    img2 = copy(img)
-    for y=1:length(seam)
-        img2[seam[y], y]=color
-    end
-    img2
-end
+# using Colors
+# #Mark a seam
+# function mark_seam(img, seam, color=RGB4{U8}(1,1,1))
+#     img2 = copy(img)
+#     for y=1:length(seam)
+#         img2[seam[y], y]=color
+#     end
+#     img2
+# end
 
-function minseam(img)
-  e, dirs = least_energy(energy(brightness(img)))
-  x = indmin(e)
-  seam = get_seam(dirs,x)
-end
+# function minseam(img)
+#   e, dirs = least_energy(energy(brightness(img)))
+#   x = indmin(e)
+#   seam = get_seam(dirs,x)
+# end
     
     
-function carve(img,seam)
-    w, h = size(img)
+# function carve(img,seam)
+#     w, h = size(img)
 
 
 
-    # newdata = Array(ColorTypes.RGB4{FixedPointNumbers.UfixedBase{UInt8,8}}, size(img.data, 2), size(img.data, 1) - 1)
-    # newimg = Image(newdata)
-    newimg = copy(img)
-    newimg.data = newimg.data[1:w-1, :]
+#     # newdata = Array(ColorTypes.RGB4{FixedPointNumbers.UfixedBase{UInt8,8}}, size(img.data, 2), size(img.data, 1) - 1)
+#     # newimg = Image(newdata)
+#     newimg = copy(img)
+#     newimg.data = newimg.data[1:w-1, :]
 
-    for y=1:h
-        s=seam[y]
-        newimg[:,y] = img[[1:s-1;s+1:end],y] # delete pixel x=s on row y
-    end
-    newimg
-end
+#     for y=1:h
+#         s=seam[y]
+#         newimg[:,y] = img[[1:s-1;s+1:end],y] # delete pixel x=s on row y
+#     end
+#     newimg
+# end
     
-carve(img) = carve(img, minseam(img))  
+# carve(img) = carve(img, minseam(img))  
 
-function carve(img, n::Int)
-  for i=1:n
-    img2 = carve(img2)
+# function carve(img, n::Int)
+#   for i=1:n
+#     img2 = carve(img2)
         
+#   end
+#   img2
+# end
+
+function all_carvings(img::Image)
+  A=[img for i=1:1] # set up a vector of images
+  C = CarvableImage(img)
+  for i=1:size(img,1)-1
+    carve!(C)
+    push!(A, get_image(C))
+    # push!(A,carve(A[end]))
   end
-  img2
+  A
 end
 
-function all_carvings(img)
-  println("When we reach $(size(img,1)-1) we have carved the image down to 1 pixel wide:")
-  A=[img for i=1:1] # set up a vector of images
-  @time (C = CarvableImage(img);
-    for i=1:size(img,1)-1
-      carve!(C)
-      push!(A, get_image(C))
-      # push!(A,carve(A[end]))
-      if(rem(i,5)==0) || i==size(img,1)-1 print(i, " ") end
-    end
-    )
-  # Profile.print()
-  A
+function save_expected_data()
+  if ~isfile("320px-Broadway_tower_edit.jpg")
+    run(`wget https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Broadway_tower_edit.jpg/320px-Broadway_tower_edit.jpg`)
+  end
+  img = imread("320px-Broadway_tower_edit.jpg")
+  A = all_carvings(img)
+  outfile = open("tower_carves.dat", "w")
+  serialize(outfile, A)
+  close(outfile)
 end
 
 function verify(carved_images)
